0b9ff236f0c0c2811721549b11dd88e7f40a6e40
Alex Perrone <alexander.perrone@jhuapl.edu>
2016-11-09 23:29:06 -0500

    exploratory data analysis on Raleigh data in an Rnotebook. also some quick modelling.

--- raleigh/code/11-read.R
+++ raleigh/code/11-read.R
@@ -5,7 +5,9 @@ library(readr)
 library(magrittr)
 library(lubridate)
 
-# Read in data. Using readr::read_csv seems better on these data (fread has errors). 
+ 
+#  Read in data. ----------------------------------------------------------
+# Using readr::read_csv seems better on these data (fread has errors). 
 inspection <- read_csv("raleigh/data/Food_Inspections.csv") %>% 
   data.table()
 violation <- read_csv("raleigh/data/Food_Inspection_Violations.csv") %>% 
@@ -29,20 +31,48 @@ setdiff(violation$HSISID, inspection$HSISID)
 setdiff(inspection$HSISID, restaurant_info$HSISID) %>% length()  # > 1,000 mis-matches
 setdiff(restaurant_info$HSISID, inspection$HSISID) %>% length()
 
+# Violation codes ---------------------------------------------------------
+# Quick insight into violation codes. Because there are so many codes, it doesn't
+# make much sense to keep them around, such as in a wide format (and the 
+# long format is inconvenient because it is not one inspection per row).  
+violation[ , unique(ViolationCode)]  # show all codes , ~ 300 of them.
+violation[ , .N, by = ViolationCode][order(-N)] %>% 
+  write_csv(path = "raleigh/data/violation-freq.csv")  # write out frequencies
 
-# Merge inspections and violations. 
-dat <- merge(inspection, violation, by.x = c("HSISID", "Date"), 
+# Each violation code can take on different levels of `critical`, suggesting
+# it is not the code that is critical or not, but how severely code was violated.
+violation[ , .N, by = .(critical, ViolationCode)][order(ViolationCode)]
+
+# Aggregate violations to the inpsection date. 
+violation_agg <- violation[ , .(num_critical = length(which(critical == "Yes")), 
+               num_non_critical = length(which(critical == "No" | is.na(critical)))),
+           by = .(HSISID, InspectDate, InspectedBy)]
+
+
+# Merge inspections and violations.  --------------------------------------
+dat <- merge(inspection, violation_agg, by.x = c("HSISID", "Date"), 
              by.y = c("HSISID", "InspectDate"), all.x = TRUE)  # left join
-# Examine further the mis-matches: those that are in inspection, not in restaurant_info. 
+# We note that in `violation_agg`, there are no NA `num_critical` or 
+# `num_non_critical`. So if there are NA values for these columns in dat, 
+# that means there was no violation that matched that inspection. However, 
+# it does not appear they had no violations, as scores are not that different
+# from ones that did match. 
+dat[is.na(num_critical), mean(Score)]
+dat[!is.na(num_critical), mean(Score)]
+# Because of above, we remove any inspections that do not have violation info. 
+# Note: this is equivalent to doing left-join above. 
+dat <- subset(dat, !is.na(num_critical) & !is.na(num_non_critical))
+all.equal(dat, merge(inspection, violation_agg, by.x = c("HSISID", "Date"), 
+                by.y = c("HSISID", "InspectDate")))  # equivalent to left-join
+
+
+# Merge insections and violations with restaurant info.  ------------------
+# Examine mis-matches first: those in inspection, not in restaurant_info. 
 dat[!(HSISID %in% restaurant_info$HSISID), ]
-# We note that in `violation`, there are no ViolationCode's that are NA. So in 
-# the merged dataset, if the ViolationCode is NA, it is because there was no 
-# matching violation, so we set the value of the ViolationCode to "none", i.e., 
-# there was an inspection with no violation. 
-violation[is.na(ViolationCode)]  # empty
-violation[ , unique(ViolationCode)]  # show all codes 
-dat[is.na(ViolationCode) , ViolationCode := "none"]
+dat <- merge(dat, restaurant_info, by = "HSISID")  # perform merge (inner join)
 
-# Merge insections and violations with restaurant info. 
-dat <- merge(dat, restaurant_info, by = "HSISID")
+# Remove scores of zero. 
+dat <- subset(dat, Score != 0)
 
+# Write out the data. 
+write_csv(dat, path = "raleigh/data/merged.csv")
