475c70e08e7016fcc03ff6dd552ee56f0e8d2c0c
Alicia M Bargar <Alicia.Bargar@jhuapl.edu>
2016-11-14 10:50:09 -0500

    parsed food categories from general Yelp list.  including parsing files in case we want to further specify or generalize which categories are picked out.

---
+++ yelpWork/parseYelpCategories.py
@@ -0,0 +1,35 @@
+
+import json
+from pprint import pprint
+import csv
+
+
+def getSelfAndChildren(self):
+
+    result = [ self ]
+    if self in parentSet:
+        result.extend([ childresult for child in parentDict[self] for childresult in getSelfAndChildren(child)])
+        return result
+    else:
+        return result
+        
+
+with open('categories.json') as data_file:
+    data = json.load(data_file)
+
+    parents = [ str(cat['parents'][0]) if len(cat['parents']) > 0 else '' for cat in data]
+
+    parentSet = set(parents)
+    parentDict = { p: [] for p in parentSet }
+
+    for i in range(len(data)):
+        parentDict[ parents[i] ].append( str(data[i]['alias']) )
+
+    foodCategories = [ catresult for cat in ("restaurants", "food", "bars", "coffeeshops") for catresult in getSelfAndChildren(cat)]
+
+    with open("foodCategories.csv", 'w') as foodcsv:
+            writer = csv.writer(foodcsv, delimiter=",")
+            for cat in foodCategories:
+                writer.writerow([cat])
+    
+
