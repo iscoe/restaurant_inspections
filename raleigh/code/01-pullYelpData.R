dda06c9d603c264d1d0fd9d1d421a928faaaba06
Alicia M Bargar <Alicia.Bargar@jhuapl.edu>
2016-11-15 07:59:25 -0500

    saving current status of pullYelpData before merge

--- raleigh/code/01-pullYelpData.R
+++ raleigh/code/01-pullYelpData.R
@@ -1,8 +1,30 @@
 library(httr)
 library(jsonlite)
 library(dplyr)
+library(plyr)
 library(readr)
 library(data.table)
+
+writeData <- function(yelpDF, categoryDF){
+  write.table(yelpDF, file = "yelpData.csv", append = TRUE, row.names = TRUE, sep = ",")
+  #all_false_cols <- apply(categoryDF, MARGIN = 2, function(x){ all(x == FALSE)})
+  #existingCategoryDF <- categoryDF[, !all_false_cols]
+  write.table(categoryDF, file = "yelpRestaurantCategories.csv", append = TRUE, row.names = TRUE, sep = ",")
+}
+
+accessToken <- "sDLP7PHbl53ruD4taAbSUl3kezQED4blRTEuSRPvf0w5a7C9nrndLl1R8sl4_FzoFZDQoN_Jhl1YuU-EmIdg_lh9zQZrx-pVpEOXV9tKsWmzIrdVsu9jJ_KPeN0kWHYx"
+yelpUrl <- "https://api.yelp.com/v3/businesses/search?"
+features = c("name",
+             "is_closed",
+             "rating",
+             "review_count",
+             "location.address1",
+             "location.zip_code",
+             "coordinates.latitude",
+             "coordinates.longitude",
+             "price",
+             "phone")
+
 
 restaurant_names <- read_csv("raleigh/data/Restaurants_in_Wake_County.csv") %>% 
   data.table() %>%
@@ -10,30 +32,15 @@ restaurant_names <- read_csv("raleigh/data/Restaurants_in_Wake_County.csv") %>%
                 X,
                 Y)
 
-#appID <- "LKSRchDCQUVpr5Z_0V0s7A"
-#appSecret <- "HHOjlap3IqRl9PDss7S1ZJclKJYjBUk8QEcirMH9SxNUVr622EZnrgg6tRDSll0f"
-accessToken <- "sDLP7PHbl53ruD4taAbSUl3kezQED4blRTEuSRPvf0w5a7C9nrndLl1R8sl4_FzoFZDQoN_Jhl1YuU-EmIdg_lh9zQZrx-pVpEOXV9tKsWmzIrdVsu9jJ_KPeN0kWHYx"
+yelpCategories <- read_csv("yelpWork/foodCategories.csv")$CATEGORIES
+numCats <- length(yelpCategories)
 
-yelpUrl <- "https://api.yelp.com/v3/businesses/search?"
-term <- paste0("term=", "restaurants")
-location <- paste0("location=", "21045")
-limit <- paste0("limit=", "50")
-
-yelpList <- list()
-yelpCategories <- list()
-
-features = c("name",
-"is_closed",
-"rating",
-"review_count",
-"location.address1",
-"location.zip_code",
-"coordinates.latitude",
-"coordinates.longitude",
-"price",
-"phone")
+categoryDF <-setNames(data.frame(matrix(ncol = numCats, nrow = 0)), yelpCategories)
+yelpDF <- setNames(data.frame(matrix(ncol = length(features), nrow = 0)), features)
 
 for(i in 1:nrow(restaurant_names)){
+  print("On restaurant #")
+  print(i)
   row <- restaurant_names[i,]
   #Tricky -doesn't handle spaces but we need to ensure we get the actual restaurant.  Using first word.
   #You can't merely delete the spaces because if there's a number after the name (indicating one of multiple
@@ -41,12 +48,10 @@ for(i in 1:nrow(restaurant_names)){
   restaurantName <-sub(" [0-9]", "", sub("\\#.*", "", row$Name))
   paramName <- gsub("\\ ", "%20", restaurantName)
   params <- paste0("term=restaurants,", paramName, "&",
-                    "latitude=", row$Y, "&", 
+                    "latitude=", row$Y, "&",
                    "longitude=", row$X, "&",
-                   #"raidus=", "50", "&",
                    "sort_by=", "best_match", "&",
                    "limit=", "20")
-  
   location_data <- GET(paste0(yelpUrl, params),
                        add_headers(Authorization = paste0("Bearer ",accessToken)))
   location_content <- content(location_data, type = "text");
@@ -56,11 +61,30 @@ for(i in 1:nrow(restaurant_names)){
       #sometimes not all of the columns get returned back (ie price).
       availableFeatures <- intersect(features, colnames(bus_df))
       df <- bus_df %>%
-          dplyr::select(one_of(availableFeatures));
-      
-      yelpList[[i]] <- df;
+           dplyr::select(one_of(availableFeatures));
+      bus_cats <- bus_df[,c("id","categories")];
+      bus_categoryDF <- setNames(data.frame(matrix(FALSE, ncol = numCats, nrow = length(bus_cats$id))), yelpCategories)
+      row.names(df) <- bus_df$id
+      row.names(bus_categoryDF) <- bus_df$id
+      for(i in 1:nrow(bus_cats)){
+        id <- bus_cats[i,]$id
+        cats <- intersect(bus_cats[i,]$categories[[1]]$alias, yelpCategories)
+        bus_categoryDF[id,cats] <- TRUE
+      }
+      #print(colnames(categoryDF))
+      #print(colnames(bus_categoryDF))
+      categoryDF <- rbind.fill(categoryDF, bus_categoryDF)
+      #print(colnames(yelpDF))
+      #print(colnames(df))
+      yelpDF <- rbind.fill(yelpDF, df);
+  }
+  if(i %% 50 == 0){
+    writeData(yelpDF, categoryDF)
+    categoryDF <-setNames(data.frame(matrix(ncol = numCats, nrow = 0)), yelpCategories)
+    yelpDF <- setNames(data.frame(matrix(ncol = length(features), nrow = 0)), features)
   }
 }
 
-yelpDF <- rbindlist(yelpList, use.names=TRUE, fill=TRUE, idcol=NULL)
-write.csv(yelpDF[!duplicated(yelpDF),], file = "yelpData.csv")
+writeData(yelpDF, categoryDF)
+
+#once this work is done, need to also just keep the actual restaurants.
