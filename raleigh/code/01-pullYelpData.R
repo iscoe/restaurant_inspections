b0ca7552e43863f06bdd588ca2454d078d3819fd
Alicia M Bargar <Alicia.Bargar@jhuapl.edu>
2016-11-16 06:45:08 -0500

    fixing merge

--- raleigh/code/01-pullYelpData.R
+++ raleigh/code/01-pullYelpData.R
@@ -1,8 +1,31 @@
 library(httr)
 library(jsonlite)
 library(dplyr)
+library(plyr)
 library(readr)
 library(data.table)
+
+writeData <- function(yelpList, categoryList){
+  yelpDF <- rbindlist(yelpList, use.names=TRUE, fill=TRUE, idcol=NULL)
+  yelpDF <- unique(yelpDF)  # delete duplicates
+  write.table(yelpDF, file = "yelpData.csv", append = TRUE, row.names = TRUE, sep = ",")
+  categoryDF <- rbindList(categoryList, use.names=TRUE, fill=TRUE< idcol=NULL)
+  write.table(categoryDF, file = "yelpRestaurantCategories.csv", append = TRUE, row.names = TRUE, sep = ",")
+}
+
+accessToken <- "sDLP7PHbl53ruD4taAbSUl3kezQED4blRTEuSRPvf0w5a7C9nrndLl1R8sl4_FzoFZDQoN_Jhl1YuU-EmIdg_lh9zQZrx-pVpEOXV9tKsWmzIrdVsu9jJ_KPeN0kWHYx"
+yelpUrl <- "https://api.yelp.com/v3/businesses/search?"
+features = c("name",
+             "is_closed",
+             "rating",
+             "review_count",
+             "location.address1",
+             "location.zip_code",
+             "coordinates.latitude",
+             "coordinates.longitude",
+             "price",
+             "phone")
+
 
 restaurant_names <- read_csv("raleigh/data/Restaurants_in_Wake_County.csv") %>% 
   data.table() %>%
@@ -10,28 +33,11 @@ restaurant_names <- read_csv("raleigh/data/Restaurants_in_Wake_County.csv") %>%
                 X,
                 Y)
 
-#appID <- "LKSRchDCQUVpr5Z_0V0s7A"
-#appSecret <- "HHOjlap3IqRl9PDss7S1ZJclKJYjBUk8QEcirMH9SxNUVr622EZnrgg6tRDSll0f"
-accessToken <- "sDLP7PHbl53ruD4taAbSUl3kezQED4blRTEuSRPvf0w5a7C9nrndLl1R8sl4_FzoFZDQoN_Jhl1YuU-EmIdg_lh9zQZrx-pVpEOXV9tKsWmzIrdVsu9jJ_KPeN0kWHYx"
+yelpCategories <- read_csv("yelpWork/foodCategories.csv")$CATEGORIES
+numCats <- length(yelpCategories)
 
-yelpUrl <- "https://api.yelp.com/v3/businesses/search?"
-term <- paste0("term=", "restaurants")
-location <- paste0("location=", "21045")
-limit <- paste0("limit=", "50")
-
-yelpList <- list()
-yelpCategories <- list()
-
-features = c("name",
-"is_closed",
-"rating",
-"review_count",
-"location.address1",
-"location.zip_code",
-"coordinates.latitude",
-"coordinates.longitude",
-"price",
-"phone")
+yelpList <- list();
+categoryList <- list();
 
 num_restaurants <- nrow(restaurant_names)
 
@@ -44,12 +50,10 @@ for(i in 1:num_restaurants){
   restaurantName <-sub(" [0-9]", "", sub("\\#.*", "", row$Name))
   paramName <- gsub("\\ ", "%20", restaurantName)
   params <- paste0("term=restaurants,", paramName, "&",
-                    "latitude=", row$Y, "&", 
+                    "latitude=", row$Y, "&",
                    "longitude=", row$X, "&",
-                   #"raidus=", "50", "&",
                    "sort_by=", "best_match", "&",
                    "limit=", "20")
-  
   location_data <- GET(paste0(yelpUrl, params),
                        add_headers(Authorization = paste0("Bearer ",accessToken)))
   location_content <- content(location_data, type = "text", encoding = "UTF-8");
@@ -64,13 +68,23 @@ for(i in 1:num_restaurants){
       # Extract categories as comma-separated string (will be split later).
       df$categories <- unlist(lapply(bus_df$categories, 
                                      function(x) paste0(x$alias, collapse=",")))
-      
+      bus_cats <- bus_df[,c("id","categories")];
+      bus_categoryDF <- setNames(data.frame(matrix(FALSE, ncol = numCats, nrow = length(bus_cats$id))), yelpCategories)
+      row.names(df) <- bus_df$id
+      row.names(bus_categoryDF) <- bus_df$id
+      for(i in 1:nrow(bus_cats)){
+        id <- bus_cats[i,]$id
+        cats <- intersect(bus_cats[i,]$categories[[1]]$alias, yelpCategories)
+        bus_categoryDF[id,cats] <- TRUE
+      }
+      yelpCategories[[i]] <- bus_categoryDF;
       yelpList[[i]] <- df;
+ 
+  if(i %% 50 == 0){
+    writeData(yelpList, categoryList)
+    yelpList <- list();
+    categoryList <- list();
   }
 }
 
-yelpDF <- rbindlist(yelpList, use.names=TRUE, fill=TRUE, idcol=NULL)
-yelpDF <- unique(yelpDF)  # delete duplicates
-
-# Write out data. 
-write_csv(yelpDF, "yelpData2.csv")
+writeData(yelpList, categoryList)
